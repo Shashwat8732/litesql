42af9603d0902f5ac331d8060671ed41e41b1f32
Shashwat Raj <Shashwatraj8732@gmail.com>
2026-03-07 12:48:23 +0530

    Update LoginPage.js

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -56,7 +56,14 @@ function LoginPage({ onLogin }) {
       <div className="login-box">
         <div className="login-header">
           <h1>
-            <span className="login-logo"></span>
+            <span className="login-logo">
+              {/* Database SVG Icon */}
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <ellipse cx="12" cy="5" rx="9" ry="3"/>
+                <path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"/>
+                <path d="M3 12c0 1.66 4 3 9 3s9-1.34 9-3"/>
+              </svg>
+            </span>
             LiteSQL
           </h1>
           <p>Lightweight SQL Database Engine</p>
